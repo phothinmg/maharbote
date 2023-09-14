17b8bf3b5af218635acd037bfca1481fb424054b
phothinmg <phothinmg@gmail.com>
2023-09-15 00:16:45 +0630

    first commit

---
+++ src/index.jsx
@@ -0,0 +1,20 @@
+import { render } from 'preact';
+import { LocationProvider, Router, Route } from 'preact-iso';
+import  Home  from './pages/Home/index.jsx';
+import { NotFound } from './pages/_404.jsx';
+import './style.css';
+
+export function App() {
+	return (
+		<LocationProvider>
+			<main>
+				<Router>
+					<Route path="/" component={Home} />
+					<Route default component={NotFound} />
+				</Router>
+			</main>
+		</LocationProvider>
+	);
+}
+
+render(<App />, document.getElementById('app'));
