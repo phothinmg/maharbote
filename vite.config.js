906fb8c444d0ecf77b9fb58b30cf05df3256c7ef
phothinmg <phothinmg@gmail.com>
2023-09-17 02:49:40 +0630

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite';
 import preact from '@preact/preset-vite';
+import Pages from 'vite-plugin-pages';
+import generateSitemap from 'vite-plugin-pages-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [preact()],
+	plugins: [
+		preact(),
+		Pages({
+			onRoutesGenerated: routes => (generateSitemap({ routes })),
+		})
+		
+	],
 	build: {
 		sourcemap: true, 
 		minify: true, 
