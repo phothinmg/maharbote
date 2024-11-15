b5ba24e8397971a9fc82bebde515f26a8ede5fb2
phothinmg <phothinmg@gmail.com>
2024-11-15 19:57:03 +0630

    modified:   src/index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,23 +1,24 @@
-import { render } from 'preact';
-import { LocationProvider, Router, Route } from 'preact-iso';
-import Home from './pages/Home/index.jsx';
-import { NotFound } from './pages/_404.jsx';
-import Maharbote from './pages/maharbote/index.jsx';
-import Footer from './components/Footer.jsx';
+import { render } from "preact";
+import { LocationProvider, Router, Route } from "preact-iso";
+import Home from "./pages/Home/index.jsx";
+import { NotFound } from "./pages/_404.jsx";
+import Footer from "./components/Footer.jsx";
 export function App() {
-	return (
-		<LocationProvider>
-			<main>
-				<Router>
-					<Route path="/" component={Home} />
-					<Route path="/maharbote" component={Maharbote} />
-					<Route default component={NotFound} />
-				</Router>
-			</main>
-			<br /><br /><br /><br />
-			<Footer/>
-		</LocationProvider>
-	);
+  return (
+    <LocationProvider>
+      <main>
+        <Router>
+          <Route path="/" component={Home} />
+          <Route default component={NotFound} />
+        </Router>
+      </main>
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </LocationProvider>
+  );
 }
 
-render(<App />, document.getElementById('app'));
+render(<App />, document.getElementById("app"));
